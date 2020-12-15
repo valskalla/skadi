d77f9e642cb37f55911d20ae5c02c94ef6624673
skolbasov <whoisliar@gmail.com>
2020-12-15 16:30:05 +0200

    Use AsCarrier type class in opentracing

--- opentracing/src/main/scala/io/skadi/opentracing/TracerBuilder.scala
+++ opentracing/src/main/scala/io/skadi/opentracing/TracerBuilder.scala
@@ -5,7 +5,7 @@ import cats.syntax.all._
 import io.opentracing.propagation.{Format, TextMap, TextMapAdapter}
 import io.opentracing.{SpanContext, Tracer => OTracer}
 import io.skadi.opentracing.impl.{OpentracingContext, OpentracingSpan, OpentracingTracer}
-import io.skadi.{Context, Span, Trace, TraceCarrier, Tracer, TracerClock}
+import io.skadi.{AsCarrier, Context, Span, Trace, TraceCarrier, Tracer, TracerClock}
 
 /**
   * Builder of `io.skadi.Tracer` that creates a combination of `Tracer` and `TraceCarrier` as a result.
@@ -16,35 +16,40 @@ class TracerBuilder[F[_]](openTracer: OTracer)(implicit F: Sync[F], t: Trace[F],
   /**
     * Tracer that specifically deals with HTTP headers as carrier
     */
-  def throughHttpHeaders: Tracer[F] with TraceCarrier[F, Map[String, String]] = throughMap(Format.Builtin.HTTP_HEADERS)
+  def throughHttpHeaders[Carrier](
+      implicit asCarrier: AsCarrier[Map[String, String], Carrier]
+  ): Tracer[F] with TraceCarrier[F, Carrier] = throughMap(Format.Builtin.HTTP_HEADERS)
 
   /**
     * Tracer that works with any general Text map as carrier
     */
-  def throughTextMap: Tracer[F] with TraceCarrier[F, Map[String, String]] = throughMap(Format.Builtin.TEXT_MAP)
+  def throughTextMap[Carrier](implicit asCarrier: AsCarrier[Map[String, String], Carrier]): Tracer[F] with TraceCarrier[F, Carrier] =
+    throughMap(Format.Builtin.TEXT_MAP)
 
-  private def throughMap(format: Format[TextMap]): Tracer[F] with TraceCarrier[F, Map[String, String]] =
-    new OpentracingTracer[F](openTracer) with TraceCarrier[F, Map[String, String]] {
-      def fromCarrier(carrier: Map[String, String]): F[Option[Context]] = F.delay {
-        openTracer.extract(format, new TextMapAdapter(smapToJava(carrier))) match {
+  private def throughMap[Carrier](
+      format: Format[TextMap]
+  )(implicit asCarrier: AsCarrier[Map[String, String], Carrier]): Tracer[F] with TraceCarrier[F, Carrier] =
+    new OpentracingTracer[F](openTracer) with TraceCarrier[F, Carrier] {
+      def fromCarrier(carrier: Carrier): F[Option[Context]] = F.delay {
+        openTracer.extract(format, new TextMapAdapter(smapToJava(asCarrier.to(carrier)))) match {
           case null => None
           case spanContext: SpanContext =>
             Some(OpentracingContext(spanContext))
         }
       }
 
-      def getCarrier: F[Option[Map[String, String]]] = t.getSpan.flatMap {
+      def getCarrier: F[Option[Carrier]] = t.getSpan.flatMap {
         case Some(span) => toContext(span).map(Some(_))
         case _          => F.pure(None)
       }
 
-      private def toContext(span: Span): F[Map[String, String]] =
+      private def toContext(span: Span): F[Carrier] =
         span match {
           case opentracingSpan: OpentracingSpan =>
             F.delay {
               val map = new java.util.HashMap[String, String]()
               openTracer.inject(opentracingSpan.underlying.context(), Format.Builtin.TEXT_MAP, new TextMapAdapter(map))
-              jmapToScala(map)
+              asCarrier.from(jmapToScala(map))
             }
           case _ =>
             F.raiseError(IllegalSpanKind)
